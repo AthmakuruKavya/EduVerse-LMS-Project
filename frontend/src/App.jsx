3f1b9d5b2d41b577d04a53800f89d1fbc7300665
AthmakuruKavya <athmakurukavya8@gmail.com>
2025-10-31 11:02:18 +0530

    create base setup for frontend and backend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+
+  return (
+    <>
+      <Button className="bg bg-red-600">Hiii..</Button>
+    </>
+  )
+}
+
+export default App
